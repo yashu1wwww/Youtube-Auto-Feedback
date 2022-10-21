84d444833f55717a7e3d241c7b7a25fd75e1fb4f
Yashu_Krish11 <89150253+yashu1wwww@users.noreply.github.com>
2022-10-21 16:17:30 +0530

    Update fd.py

--- fd.py
+++ fd.py
@@ -7,8 +7,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 import time
 
-email = 'mossa100q\n'             #change to your mail
-password = 'pas2@#$%\n'           #change to your pass  
+email = 'mail23\n'               #change to your mail
+password = 'pass23@\n'           #change to your pass  
 
 driver = uc.Chrome(use_subprocess=True)
 wait = WebDriverWait(driver,20)
@@ -23,7 +23,7 @@ driver.find_element_by_id("avatar-btn").click()
 time.sleep(10)
 driver.find_element_by_xpath('/html/body/ytd-app/ytd-popup-container/tp-yt-iron-dropdown/div/ytd-multi-page-menu-renderer/div[3]/div[1]/yt-multi-page-menu-section-renderer[5]/div[2]/ytd-compact-link-renderer[2]/a/tp-yt-paper-item').click()
 time.sleep(3)
-driver.find_element_by_css_selector('body > div:nth-child(3) > div > div > uf-describe-page > form > div:nth-child(2) > textarea').send_keys("hey youtube")
+driver.find_element_by_css_selector('body > div:nth-child(3) > div > div > uf-describe-page > form > div:nth-child(2) > textarea').send_keys("hey youtube") #change to your feedback text
 time.sleep(1)
 driver.find_element_by_xpath('/html/body/div[2]/div/div/uf-describe-page/form/footer/uf-material-button[1]/label/button').click()
 time.sleep(30)
